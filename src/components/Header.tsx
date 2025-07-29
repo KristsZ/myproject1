7b6b4e02765c7a1a7e66ce33e89f67fac6f38f5c
KristsZ <krists.zarans@gmail.com>
2025-07-29 18:31:00 +0300

    upadet2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Dancing_Script } from "next/font/google";
-import Image from 'next/image';
 
 const dancingScript = Dancing_Script({
   subsets: ["latin"],
